f7b594ec9c03fbb5914cc2372f602f2b7cd96239
Juri Pakaste <juri.pakaste@richie.fi>
2024-02-15 14:41:33 +0200

    Package.swift 202402.25.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "richiesdks-ios",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v15),
     ],
     products: [
         .library(
@@ -20,13 +20,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RichieSDK",
-            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202402.24.0/richiesdk-ios-202402.24.0.zip",
-            checksum: "e8d191f55a67cbf4350095751199756655a9678c290fb40f6bc43103013d5f0a"
+            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202402.25.0/richiesdk-ios-202402.25.0.zip",
+            checksum: "b1eda11de218ff34e51d744bfa77ef2240aa7522dada3cb0caa7dc0879b547c3"
         ),
         .binaryTarget(
             name: "RichieEditionsSDK",
-            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202402.24.0/richieeditionssdk-ios-202402.24.0.zip",
-            checksum: "69755dacd737b0c2d322fe7db4e639ff84a5fa80d523effc7602a0142e298782"
+            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202402.25.0/richieeditionssdk-ios-202402.25.0.zip",
+            checksum: "93cccc81278250c913e187eafe5aa050c700ddb92102ccf26590dff8dadd31af"
         ),
     ]
 )
