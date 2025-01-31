80df69bf015ea1eee705497c2c19f4e640f8f758
Luis Ángel San Martín <luisangelsm@gmail.com>
2025-01-31 09:34:41 +0100

    Package.swift 202501.30.0

--- Package.swift
+++ Package.swift
@@ -20,13 +20,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RichieSDK",
-            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202411.29.0/richiesdk-ios-202411.29.0.zip",
-            checksum: "e0ab0c27c8e87f8ad1cff98435eac2ddc532c101e2cb63dce8926946a17dd015"
+            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202501.30.0/richiesdk-ios-202501.30.0.zip",
+            checksum: "0287f79d9320e7b50720944438ab557ffbefb8f0f8170004d8ffb92adcd711c8"
         ),
         .binaryTarget(
             name: "RichieEditionsSDK",
-            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202411.29.0/richieeditionssdk-ios-202411.29.0.zip",
-            checksum: "9e08b9b39204adc33fa4938f75855475989f69ff1638b467bed98a5b94fce1b5"
+            url: "https://github.com/richiefi/richiesdk-ios/releases/download/202501.30.0/richieeditionssdk-ios-202501.30.0.zip",
+            checksum: "1a6e8c6ab34171b528a5cee754b13e592de6109f32864c766b89fcb48bd7fcaa"
         ),
     ]
 )
